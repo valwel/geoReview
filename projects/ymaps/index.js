dd098b4040cdc9c6284b6b77e333f607389b0a61
Valentin Shevchenko <valwel@bk.ru>
2022-03-04 18:38:43 +0300

    initial commit

--- projects/ymaps/index.js
+++ projects/ymaps/index.js
@@ -11,7 +11,7 @@ function init() {
     zoom: 12,
   });
   let clusterer = new ymaps.Clusterer({
-    groupByCoordinates: false,
+    groupByCoordinates: true,
     clusterDisableClickZoom: true,
     clusterOpenBalloonOnClick: false,
   });
@@ -46,14 +46,15 @@ function init() {
   function openBalloon(coords, withReviews) {
     const reviews = withReviews ? getReveiwsByCoords(coords) : null;
     myMap.balloon.open(coords);
-    const form = createForm(coords, withReviews, reviews);
+    const form = createForm(coords, reviews);
     myMap.balloon.setData(form.innerHTML);
   }
   myMap.events.add('click', function (e) {
-    openBalloon(e.get('coords'));
+    let coords = e.get('coords')
+    openBalloon(coords);
   });
 
-  function createForm(coords, withReviews, reviews) {
+  function createForm(coords, reviews) {
     const root = document.createElement('div');
     const list = document.createElement('div');
     root.innerHTML = balloonContent;
@@ -72,24 +73,26 @@ function init() {
         list.appendChild(div);
       });
       root.prepend(list);
-    }
-    withReviews && (addButton.dataset.role = 'addReview');
+    };
     return root;
   }
 
   document.addEventListener('click', (e) => {
     if (e.target.id === 'myButton') {
       const coords = JSON.parse(e.target.dataset.coords);
-      const role = e.target.dataset.role;
-      if (role !== 'addReview') {
-        createPlacemark(coords)
-      }
-
+      const placemark = new ymaps.Placemark(coords);
+      console.log(coords)
+      placemark.events.add('click', (e) => {
+        openBalloon(e.get('target').geometry.getCoordinates(), true);
+      });
+      clusterer.add(placemark);
+       
       const reveiw = {
         name: document.querySelector('#name').value,
         place: document.querySelector('#place').value,
         comment: document.querySelector('#comment').value,
         point: coords,
+        
       };
       points.push(reveiw);
       setToLocalStorage(localStorageKey, points);
@@ -102,14 +105,4 @@ function init() {
       (review) => review.point[0] === point[0] && review.point[1] === point[1]
     );
   }
-  function createPlacemark(coords) {
-  const placemark = new ymaps.Placemark(coords);
-        placemark.events.add('click', (e) => {
-          openBalloon(e.get('target').geometry.getCoordinates(), true);
-        });
-        clusterer.add(placemark);
 }
-}
-
-
-
